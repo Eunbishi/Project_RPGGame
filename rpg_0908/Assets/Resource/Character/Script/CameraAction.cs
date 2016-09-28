cd743d17e14918a90cc9b3369e57906be8f9bc55
Eunbi <ebstyle7@naver.com>
2016-09-28 10:54:42 +0900

    신규 프로젝트 시작

---
+++ rpg_0908/Assets/Resource/Character/Script/CameraAction.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraAction : MonoBehaviour 
+{
+    public GameObject Camera;
+
+    void Start()
+    {
+        this.transform.position = Camera.transform.position;
+    }
+
+    void Update()
+    {
+        this.transform.position = Camera.transform.position;
+    }
+}
